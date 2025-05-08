c827bf9f56ac7ec5fa162b1cbec48a4aa5b8d8cb
ishika <ishikatakhtani04@gmail.com>
2025-05-08 23:49:20 +0530

    updation

---
+++ src/components/Header.jsx
@@ -0,0 +1,79 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+const Header=()=>{
+    return(
+        <>
+       
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand style={{marginRight:"10%"}} href="#">CONVERSE </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            
+            <Nav.Link style={{fontWeight:"bolder", color:"black"}} as={Link} to="Home" >New &Featured</Nav.Link>
+            <Nav.Link  style={{fontWeight:"bolder", color:"black"}} as={Link} to="Home">Women</Nav.Link>
+            <Nav.Link style={{fontWeight:"bolder", color:"black"}} as={Link} to="Home">Men</Nav.Link>
+            <Nav.Link style={{fontWeight:"bolder", color:"black"}} as={Link} to="Home">Kids</Nav.Link>
+            <Nav.Link style={{fontWeight:"bolder", color:"black"}} as={Link} to="Home">Limited Edition</Nav.Link>
+            <Nav.Link style={{color:"red",fontWeight:"bolder"}} as={Link} to="Home">Sale </Nav.Link>
+            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown> */}
+            
+            
+          </Nav>
+          <Nav.Link style={{paddingRight:"3%" ,fontWeight:"bolder", color:"black"}} href="#" disabled>
+              Singin
+            </Nav.Link>
+            <Nav.Link style={{paddingRight:"3%",fontWeight:"bolder", color:"black"}}href="#" disabled>
+              op2
+            </Nav.Link>
+            <Nav.Link style={{paddingRight:"3%",fontWeight:"bolder", color:"black"}}href="#" disabled>
+              op2
+            </Nav.Link>
+            <Nav.Link style={{paddingRight:"3%",fontWeight:"bolder", color:"black"}}href="#" disabled>
+              op2
+            </Nav.Link>
+            
+          <Form  className="d-flex">
+          
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              style={{fontWeight:"bolder", backgroundColor:"black"
+               
+              }}
+              
+            />
+          
+            {/* <Button variant="outline-success">Search</Button> */}
+           
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+   
+        </>
+    )
+}
+export  default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,81 @@
+const Home=()=>{
+    return(
+        <>
+        <div id="h1">
+        <img src="https://www.converse.in/media/wysiwyg/Desktop.jpg?auto=webp&format=webp&quality=70" alt="" />
+        </div>
+        <button id="b1" >Shop Now</button>
+        <div id="h2">
+            <img src="https://www.converse.in/media/wysiwyg/May_25_homepage_copy-16-1_1.jpg?auto=webp&format=webp&quality=70" alt="" height={"600px"}/>
+            <img src="https://www.converse.in/media/wysiwyg/May_22_homepage_copy-15.jpg?auto=webp&format=webp&quality=70" alt=""  height={"600px"} />
+            <img src="https://www.converse.in/media/wysiwyg/May_25_homepage_copy-17-1.jpg?auto=webp&format=webp&quality=70" alt=""  height={"600px"} />
+        </div>
+
+        <div id="h3">
+            <h2>Shop By Style</h2>
+        </div>
+        <div id="h4">
+        <div id="i1">
+            <img src="https://www.converse.in/media/wysiwyg/Frame_37_1__1.png?auto=webp&format=webp&quality=70" alt=""  />
+          <p id="d1">High Top </p></div>
+          <div id="i1">
+            <img src="https://www.converse.in/media/wysiwyg/Frame_38_4.png?auto=webp&format=webp&quality=70" alt="" />
+            <p id="d1">Low Top</p></div>
+            <div id="i1">
+            <img src="https://www.converse.in/media/wysiwyg/Frame_39_2.png?auto=webp&format=webp&quality=70" alt="" />
+            <p id="d1">PLatform</p></div>
+            <div id="i1">
+            <img src="https://www.converse.in/media/wysiwyg/Frame_40_3.png?auto=webp&format=webp&quality=70" alt="" />
+            <p id="d1">Kids</p></div>
+        </div>
+        
+        <div id="h5">
+            
+<div id="h2">
+    <img src="https://www.converse.in/media/wysiwyg/rick_owens_desktop-22-1.png?auto=webp&format=webp&quality=70" alt="" height={"700px"} />
+    <div>
+    <img src="https://www.converse.in/media/wysiwyg/rick_owens_desktop-21.png?auto=webp&format=webp&quality=70" alt="" height={"700px"} />
+   <button  id="b2">Shop now</button></div>
+    <img src="https://www.converse.in/media/wysiwyg/rick_owens_desktop-23-1.png?auto=webp&format=webp&quality=70" alt="" height={"700px"} />
+</div>
+</div>
+
+<div id="h2">
+<div>
+<img src="https://www.converse.in/media/wysiwyg/May_22_homepage_copy-07-1.jpg?auto=webp&format=webp&quality=70" alt="" />
+<button  id="b3">Shop now</button></div>
+<img src="https://www.converse.in/media/wysiwyg/May_22_homepage_copy-08-1.jpg?auto=webp&format=webp&quality=70" alt="" />
+</div>
+
+<div id="h2">
+<div id="h3">
+    <div>
+        <img src="https://www.converse.in/media/wysiwyg/May_22_homepage_copy-04.jpg?auto=webp&format=webp&quality=70" alt=""    />
+        <button id="b4">Shop now</button>
+        <img src="https://www.converse.in/media/wysiwyg/april_slicespng-05_1.png?auto=webp&format=webp&quality=70" alt=""  />
+        
+        </div>
+        <img src="https://www.converse.in/media/wysiwyg/april_slicespng-06_1.png?auto=webp&format=webp&quality=70" alt=""  />
+</div>
+</div>
+
+<div id="h3">
+<div>
+    <img src="https://www.converse.in/media/wysiwyg/May_25_homepage_copy-03-1.jpg?auto=webp&format=webp&quality=70" alt="" />
+</div>
+<div>
+    <img src="https://www.converse.in/media/wysiwyg/May_22_homepage_copy-01_1.jpg?auto=webp&format=webp&quality=70" alt="" />
+    <button id="b5">Shop now</button>
+    <img src="https://www.converse.in/media/wysiwyg/May_25_homepage_copy-02-1.jpg?auto=webp&format=webp&quality=70" alt="" />
+</div>
+</div>
+<div id="h3">
+    <img src="https://www.converse.in/media/wysiwyg/app.jpg?auto=webp&format=webp&quality=70" alt=""  height={"600px"}/>
+    <button id="b6">Shop now</button>
+    <img src="https://www.converse.in/media/wysiwyg/acc.jpg?auto=webp&format=webp&quality=70" alt="" height={"600px"}/>
+    <button id="b7">Shop now</button>
+</div>
+        </>
+    )
+}
+export default Home
