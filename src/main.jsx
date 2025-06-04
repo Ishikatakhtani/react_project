64fcbd80a5a2fe627b79a0c6945d3784a3d1cf63
ishika <ishikatakhtani04@gmail.com>
2025-06-04 17:08:17 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,36 @@
 
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
 
-import "./style.css"
+// import "./style.css"
 
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import store from './store.jsx';
-import { Provider } from 'react-redux';
+// import App from './App.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import store from './store.jsx';
+// import { Provider } from 'react-redux';
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
  
-<Provider store={store}>
-<App />
-</Provider>
+// <Provider store={store}>
+// <App />
+// </Provider>
 
     
   
-)
+// )
+
+
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Add this
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import store from './store.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <BrowserRouter> {/* ✅ Wrap App in Router */}
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
